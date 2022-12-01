8d7f6a5fb60bbe277f9702d53751cdde6db90a3f
billysco <70048321+billysco@users.noreply.github.com>
2022-12-01 09:19:25 -0500

    Updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import pickle
 
 # Pandas code to get lists of players
 wr_df = pd.read_csv("wr_final.csv")
@@ -18,14 +19,19 @@ position = st.sidebar.selectbox(
 
 # filtering df to only contain relevant columns
 wr_df = wr_df[wr_df['Year']==year].copy()
-wr_df = wr_df.drop(columns=['Unnamed: 0', '3 Cone', 'Shuttle', 'Vertical Jump', 'Position', 'PK', 'Year', 'RushAvg'])
 
+
+# TODO change this to an if else statement based on position
 player_list = wr_df['Player']
 
 player = st.sidebar.selectbox(
     "Select a player of interest", player_list 
 )
 
+filtered_df = wr_df[wr_df['Player']==player]
+filtered_df = filtered_df.drop(columns=['Unnamed: 0', 'Player', '3 Cone', 'Shuttle', 'Vertical Jump', 'Position', 'PK', 'Year', 'RushAvg'])
+
+wr_pkl = pickle.load(open('wr_pickled_initial.pkl', 'rb'))
 # round_drafted = st.sidebar.number_input("Round Drafted",1,7)
 
 # pick_drafted = st.sidebar.number_input("Round Drafted",1)
